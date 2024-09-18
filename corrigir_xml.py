bf2c61aa1f6bcf6ba6b96958fda7cf487e4e9ff6
caiobps1 <54898245+caiobps1@users.noreply.github.com>
2024-09-18 13:01:58 -0300

    Enviando corrigir_xml.py

---
+++ corrigir_xml.py
@@ -0,0 +1,19 @@
+import argparse
+import re
+
+def corrigir_xml(arquivo_xml):
+    with open(arquivo_xml, 'r', encoding='utf-8') as file:
+        conteudo = file.read()
+        conteudo = conteudo.replace('chance1', 'chance')
+        conteudo = re.sub(r' countmax="\d+"', '', conteudo)
+        conteudo = re.sub(r'-- (.*?)\s*(?=<)', r'<!-- \1 -->\n', conteudo)
+        
+    with open(arquivo_xml, 'w', encoding='utf-8') as file:
+        file.write(conteudo)
+        print(f'{arquivo_xml} corrigido.')
+
+    if __name__ == "__main__":
+        parser = argparse.ArgumentParser(description="Corrigir arquivo XML de loot.")
+        parser.add_argument("caminho_arquivo", help="Caminho do arquivo XML a ser corrigido")
+        args = parser.parse_args()
+        corrigir_xml(args.caminho_arquivo)
